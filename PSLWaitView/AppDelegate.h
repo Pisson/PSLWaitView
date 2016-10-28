960dbb9c9bc65875d6b0649b48beb1c0e29ab8c2
pisson <pisson@pissondeMacBook-Pro.local>
2016-10-28 16:31:52 +0800

    Initial Commit

---
+++ PSLWaitView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PSLWaitView
+//
+//  Created by pisson on 2016/10/28.
+//  Copyright © 2016年 pisson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PSLWaitView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PSLWaitView
+//
+//  Created by pisson on 2016/10/28.
+//  Copyright © 2016年 pisson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
